8d84095d23b11ca8ca27523ef3c883d7b273929f
a-givertzman <a.givertzman@icloud.com>
2022-09-26 23:24:15 +0300

    parse config

--- src/main.rs
+++ src/main.rs
@@ -4,66 +4,91 @@ use std::fs;
 use std::collections::HashMap;
 // use serde_json::{self, Value};
 use serde::{Serialize, Deserialize};
+use serde_with;
 
 
-#[derive(Debug, Deserialize)]
-pub struct Config {
-    _path: String,
+// #[serde_with::skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DsConfig {
     // #[serde(flatten)]
-    _lines: HashMap<String, Line>,
+    lines: HashMap<String, DsLineConf>,
 }
 
-impl Config {
-    fn new(path: String) -> Self {
-        Self{_path: path, _lines: HashMap::from([])}
-    }
-    ///
-    fn _readFromFile(&self, path: &String) -> HashMap<String, HashMap<String, serde_json::Value>> {
-        println!("reading from file: \"{}\"", &path);
-        let configJson = fs::read_to_string(&path)
-            .expect(&format!("Error read file {}", path));
-        let config: HashMap<String, HashMap<String, serde_json::Value>> = serde_json::from_str(&configJson).unwrap();
-        println!("config: {:?}", config);
-        config
-    }
-    ///
-    fn build(&mut self) {
-        let config = self._readFromFile(&self._path);
-        self._lines = serde_json::from_str(&config).unwrap();
-        // for (lineKey, line) in config {
-        //     print!("\n\t{}:\t{:?}", &lineKey, line);
-        //     self._lines.entry(lineKey).or_insert(Line::new(line));
-            // for (iedKey, ied) in line.ieds {
-            //     print!("\n\t\t{}:\t{:?}", iedKey, ied);
-            // }
-        // }    
-    }
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Line {
+#[serde_with::skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DsLineConf {
     // #[serde(flatten)]
-    _ieds: HashMap<String, Ied>,
-}
-impl Line {
-}
-
-#[derive(Debug, Clone, Deserialize)]
-pub struct Ied {
-    #[serde(flatten)]
-    _dbsConfig: HashMap<String, HashMap<String, serde_json::Value>>,
+    name: Option<String>,
+    ieds: Option<HashMap<String, DsIedConf>>,
 }
 
+#[serde_with::skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DsIedConf {
+    // #[serde(flatten)]
+    name: Option<String>,
+    ip: Option<String>,
+    rack: Option<u32>,
+    slot: Option<u32>,
+    dbs: Option<HashMap<String, DsDbConf>>,
+}
 
-#[derive(Serialize, Deserialize)]
-pub struct LineConf {
-    // ied11: Object(Map<String, Value>),
+#[serde_with::skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DsDbConf {
+    // #[serde(flatten)]
+    name: Option<String>,
+    description: Option<String>,
+    number: Option<u32>,
+    offset: Option<u32>,
+    size: Option<u32>,
+    delay: Option<u32>,
+    points: Option<HashMap<String, DsPointConf>>,
+}
+
+#[serde_with::skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DsPointConf {
+    // #[serde(flatten)]
+    dataType: Option<String>,
+    offset: Option<u32>,
+    comment: Option<String>,
+    vrt: Option<u8>,
 }
 
 fn main() {
     let dir = std::env::current_dir().unwrap();
     let path: &str = &format!("{}/conf.json", dir.to_str().unwrap());
-    let mut config = Config::new(path.to_string());// = serde_json::from_str(&configJson).unwrap();
-    println!("config: {:?}", config);
-    config.build();
+    let configJson = fs::read_to_string(&path)
+        .expect(&format!("Error read file {}", path));
+    let config: DsConfig = serde_json::from_str(&configJson).unwrap();
+    // for (key, line) in config.lines {
+    //     println!("line {:?}: {:?}", key, line);
+    // }
+    println!("config {:?}", config);
+    // config.build();
 }
+//
+// fn build(&mut self) {
+//     let configJson = self._readFromFile(&self._path);
+//     let config: HashMap<String, HashMap<String, serde_json::Value>> = serde_json::from_str(&configJson).unwrap();
+//     for (lineKey, lineConf) in config {
+//         // print!("\n\t{}:\t{:?}", &lineKey, lineConf);
+//         self._lines.insert(
+//             lineKey, 
+//             Line::new(lineConf),
+//         );
+//         // for (iedKey, ied) in line.entry("ieds") {
+//         //     print!("\n\t\t{}:\t{:?}", iedKey, ied);
+//         // }
+//     }    
+// }
+// ///
+// fn _readFromFile(&self, path: &String) -> String {
+//     println!("reading from file: \"{}\"", &path);
+//     let configJson = fs::read_to_string(&path)
+//         .expect(&format!("Error read file {}", path));
+//     println!("configJson: {:?}", configJson);
+//     configJson
+//     // : HashMap<String, HashMap<String, serde_json::Value>>
+// }
